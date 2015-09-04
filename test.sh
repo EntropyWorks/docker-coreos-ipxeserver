9c41c1ec8eb96f9e0473489ad0518ecc54fe599f
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-09-04 14:16:35 -0400

    better logging

--- test.sh
+++ test.sh
@@ -2,5 +2,11 @@ HOST=${HOST:-localhost}
 PORT=${PORT:-8085}
 HOST_URL=http://$HOST:$PORT
 echo using host: $HOST_URL
-curl $HOST_URL/coreos-ipxe/arothste/id_rsa.pub/test
-curl $HOST_URL/coreos-cloudconfig/arothste/id_rsa.pub/test
+
+IPXE_BOOT_URL=$HOST_URL/coreos-ipxe/arothste/id_rsa.pub/test
+echo iPXE boot url: $IPXE_BOOT_URL
+curl $IPXE_BOOT_URL
+
+IPXE_BOOT_CC_URL=$HOST_URL/coreos-cloudconfig/arothste/id_rsa.pub/test
+echo iPXE cloud-config url: $IPXE_BOOT_CC_URL
+curl $IPXE_BOOT_CC_URL
