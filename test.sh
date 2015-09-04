fad470b3f67d49d6e6bd715af7a3960daff89344
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-09-04 13:58:07 -0400

    better bash fu with default host and port

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
-HOST=localhost
-PORT=8085
+HOST=${HOST:-localhost}
+PORT=${PORT:-8085}
 HOST_URL=http://$HOST:$PORT
+echo using host: $HOST_URL
 curl $HOST_URL/coreos-ipxe/arothste/id_rsa.pub/test
 curl $HOST_URL/coreos-cloudconfig/arothste/id_rsa.pub/test
