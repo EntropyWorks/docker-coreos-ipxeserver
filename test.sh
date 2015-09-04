d90b6b84232cd7c76d62c750c3ded86df584f6c2
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-09-04 14:28:00 -0400

    better indentation

--- test.sh
+++ test.sh
@@ -4,9 +4,12 @@ HOST_URL=http://$HOST:$PORT
 echo using host: $HOST_URL
 
 IPXE_BOOT_URL=$HOST_URL/coreos-ipxe/arothste/id_rsa.pub/test
+
+echo
 echo iPXE boot url: $IPXE_BOOT_URL
 curl $IPXE_BOOT_URL
 
 IPXE_BOOT_CC_URL=$HOST_URL/coreos-cloudconfig/arothste/id_rsa.pub/test
+echo
 echo iPXE cloud-config url: $IPXE_BOOT_CC_URL
 curl $IPXE_BOOT_CC_URL
